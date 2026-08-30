7ca1e44bc2679dc1084d2971e2a689332c12f917
Miguel Pafume <leugim.pafume@gmail.com>
2026-08-30 10:26:11 -0300

    fix Snes file name

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    SnesApp app {argv[1]};
+    if (argv[1] == nullptr) {
+        std::cerr << "ERROR: No ROM path detected." << std::endl;
+        return 1;
+    }
 
     try {
+        SnesApp app {argv[1]};
         app.run();
     } catch (const std::exception& e) {
         std::cerr << "ERROR: " << e.what() << std::endl;
